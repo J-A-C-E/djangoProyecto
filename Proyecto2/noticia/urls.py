4390fd9f3496057ac12b509c33f67c970f597d69
J-A-C-E <60686515+J-A-C-E@users.noreply.github.com>
2022-05-29 22:49:45 -0500

    Cambios del video 41

--- Proyecto2/noticia/urls.py
+++ Proyecto2/noticia/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.noticia, name="Noticias"),
+    path('categoria/<int:categoria_id>/', views.categoria, name="categoria"),
 ]
--- Proyecto2/noticia/views.py
+++ Proyecto2/noticia/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from noticia.models import Post
+from noticia.models import Categoria, Post, Categoria
 
 # Create your views here.
 def noticia(request):
     posts = Post.objects.all()
     return render(request, "noticia/noticias.html", {"posts": posts})
+
+def categoria(request, categoria_id):
+    categoria = Categoria.objects.get(id = categoria_id)
+    posts = Post.objects.filter(categorias = categoria)
+    return render(request, "noticia/categorias.html", {'categoria':categoria, "posts": posts})
